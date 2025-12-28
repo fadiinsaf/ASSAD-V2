5e742545a5b39f34c22214f66f2410c83e48551a
fadiinsaf <fadi_man_2005@outlook.fr>
2025-12-28 22:56:55 +0100

    feat: implement comment submission handler with rating system

---
+++ Controllers/add_comment.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once __DIR__ . "/../database/database.php";
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+    $id_visit = $_POST["visit_id"];
+    $rating = (int)$_POST["rating"];
+    $comment_text = $_POST["comment_text"];
+
+    $stmt = $db->prepare("INSERT INTO comments (comment_text,rating,id_visiter,id_visit) VALUES(?,?,?,?)");
+    $status = $stmt->execute([$comment_text , $rating, $_SESSION["user_id"], $id_visit]);
+    
+    if($status){
+        header("Location: /Views/visit_details.php");
+        exit();
+    }
+
+    echo "Error";
+    die();
+}
+
+    header("Location: /Views/visit_details.php");
+    exit();
